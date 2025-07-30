51a1172ed3c62b4477ecdcc081e683331eea84f3
Abhinab-Basnet <abhinabbasnet75@gmail.com>
2025-07-30 20:46:17 +0545

    hi

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/debug/moc_dashboard.cpp
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/debug/moc_dashboard.cpp
@@ -46,6 +46,7 @@ template <> constexpr inline auto dashboard::qt_create_metaobjectdata<qt_meta_ta
         "on_Volunteer_clicked",
         "onEventChanged",
         "index",
+        "on_Booking_clicked",
         "on_viewvolunteer_clicked",
         "on_Logout_clicked",
         "on_Help_Center_clicked"
@@ -62,12 +63,14 @@ template <> constexpr inline auto dashboard::qt_create_metaobjectdata<qt_meta_ta
         QtMocHelpers::SlotData<void(int)>(5, 2, QMC::AccessPrivate, QMetaType::Void, {{
             { QMetaType::Int, 6 },
         }}),
-        // Slot 'on_viewvolunteer_clicked'
+        // Slot 'on_Booking_clicked'
         QtMocHelpers::SlotData<void()>(7, 2, QMC::AccessPrivate, QMetaType::Void),
-        // Slot 'on_Logout_clicked'
+        // Slot 'on_viewvolunteer_clicked'
         QtMocHelpers::SlotData<void()>(8, 2, QMC::AccessPrivate, QMetaType::Void),
-        // Slot 'on_Help_Center_clicked'
+        // Slot 'on_Logout_clicked'
         QtMocHelpers::SlotData<void()>(9, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_Help_Center_clicked'
+        QtMocHelpers::SlotData<void()>(10, 2, QMC::AccessPrivate, QMetaType::Void),
     };
     QtMocHelpers::UintData qt_properties {
     };
@@ -95,9 +98,10 @@ void dashboard::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, v
         case 1: _t->on_Profile_clicked(); break;
         case 2: _t->on_Volunteer_clicked(); break;
         case 3: _t->onEventChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
-        case 4: _t->on_viewvolunteer_clicked(); break;
-        case 5: _t->on_Logout_clicked(); break;
-        case 6: _t->on_Help_Center_clicked(); break;
+        case 4: _t->on_Booking_clicked(); break;
+        case 5: _t->on_viewvolunteer_clicked(); break;
+        case 6: _t->on_Logout_clicked(); break;
+        case 7: _t->on_Help_Center_clicked(); break;
         default: ;
         }
     }
@@ -122,14 +126,14 @@ int dashboard::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 7)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 7;
+        _id -= 8;
     }
     if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 7)
+        if (_id < 8)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 7;
+        _id -= 8;
     }
     return _id;
 }
